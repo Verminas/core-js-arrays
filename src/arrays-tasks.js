a75b879805d73741085e0a4a2057c2961ff588c9
verminas <veremeeva96@gmail.com>
2024-01-10 23:31:05 +0300

    feat: add soltion findCommonElements

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -481,8 +481,13 @@ function getMaxItems(/* arr, n */) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  // throw new Error('Not implemented');
+  const resultArray = arr1.map((item, index) => {
+    const newItem = arr2.includes(arr1[index]) ? arr1[index] : [];
+    return newItem;
+  });
+  return resultArray.flat();
 }
 
 /**

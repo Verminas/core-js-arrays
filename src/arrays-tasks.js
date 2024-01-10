55e8caa120b0891f69e98cba196c5da47b73aaf4
verminas <veremeeva96@gmail.com>
2024-01-10 19:19:33 +0300

    feat: add solution getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -129,8 +129,10 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  // throw new Error('Not implemented');
+  const sumArr = arr.reduce((sum, current) => sum + current, 0);
+  return arr.length > 0 ? +(sumArr / arr.length).toFixed(2) : 0;
 }
 
 /**
